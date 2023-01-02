823c2ca558d95cd5db2edc7b628041eaaa17d073
Tessa <tess.starkweather@gmail.com>
2023-01-02 12:59:21 -0500

    config sql server connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const mysql = require('mysql2');
+const sequelize = require('./config/connection');
+// const mysql = require('mysql2');
 const PORT = process.env.PORT || 3000;
 
 
@@ -9,5 +10,8 @@ app.get('/', (req, res) => {
     res.send('The server is live!');
 });
 
+sequelize.sync().then(() => {
+    app.listen(PORT)
+    console.log(`Server listening on port ${PORT}!`)
+});
 
-app.listen(PORT)
