22e4ecf2271370aa046a2ec0729fee44d0ebaca0
Tessa <tess.starkweather@gmail.com>
2023-01-11 15:18:02 -0500

    can navigate to individual posts, comment, and view comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth')
 
 // Get all comments
 router.get('/', async (req, res) => {
-    try{
+    try {
         const result = await Comment.findAll();
         const comments = result.map((comment) => {
-            return comment.get({plain: true})
+            return comment.get({ plain: true })
         })
         res.status(200).json(comments);
     }
-    catch(err) {
+    catch (err) {
         res.status(500).json(err);
     }
 });
@@ -18,12 +19,36 @@ router.get('/', async (req, res) => {
 // Create a comment
 router.post('/', async (req, res) => {
     try {
-        const newComment = await Comment.create(req.body)
+        const newComment = await Comment.create({
+            content: req.body.content,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
         res.json(newComment)
     }
-    catch(err) {
+    catch (err) {
         res.json(err);
     }
-})
+});
+
+// delete comment
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbCommentData => {
+          if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id' });
+            return;
+          }
+          res.json(dbCommentData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
 
 module.exports = router;
